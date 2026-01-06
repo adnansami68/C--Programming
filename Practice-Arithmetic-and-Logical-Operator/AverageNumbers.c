8da4a9c38c12f4e0a23d509ab1ed4b6bfc651142
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2026-01-07 02:02:27 +0600

    Average of 3 numbers
    Average of 3 numbers

---
+++ Practice-Arithmetic-and-Logical-Operator/AverageNumbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int num1 , num2 , num3;
+    float average;
+
+    printf("Enter Three Numbers:");
+
+    scanf("%d %d %d", &num1, &num2 , &num3);
+
+    average=(num1 + num2 + num3)/3.0;
+    printf("Average: %2f", average);
+
+
+    return 0;
+}
