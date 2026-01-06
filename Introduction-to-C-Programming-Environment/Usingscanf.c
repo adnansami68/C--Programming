4f9bca24fd66d1f67ac7727048d74064b6d01d4f
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2026-01-07 01:53:40 +0600

    Create Usingscanf.c
    Declare different variables and take input using scanf()

---
+++ Introduction-to-C-Programming-Environment/Usingscanf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char name[20];
+    int  age;
+    
+    printf("Enter Your Name: ");
+    scanf("%s", name);
+
+    printf("Enter Yout Age: ");
+    scanf("%d", &age);
+
+   return 0;
+}
