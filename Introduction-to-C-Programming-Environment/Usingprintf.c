1f2c8eb60abdcce43d6352f3fadf6833370709df
MD.ADNAN SAMI <a.m.adnansami143@gmail.com>
2026-01-07 01:54:22 +0600

    Add Usingprintf.c to demonstrate printf usage
    Display the output of the variables using printf().

---
+++ Introduction-to-C-Programming-Environment/Usingprintf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char name[]= "Adnan";
+    int num=75;
+    float cgpa= 3.75;
+
+    printf("Name:%s\n", name);
+    printf("Mean Number:%d\n", num);
+    printf("CGPA:%2f", cgpa);
+
+    return 0;
+
+}
